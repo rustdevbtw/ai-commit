5819130a4487a82a1c0e84e7025fea444324b23a
Rajdeep Malakar <rajdeepm.dev@gmail.com>
2024-07-08 22:47:02 +0530

    [main]: ✨ feat: add support for global and local configuration files
    Signed-off-by: Rajdeep Malakar <rajdeepm.dev@gmail.com>

--- configFile.js
+++ configFile.js
@@ -1,33 +1,41 @@
 import * as edn from "jsedn";
-import { readFile } from "fs/promises";
+import { readFile, exists } from "fs/promises";
+import { homedir } from "os";
 
-let path = process.cwd() + "/.cts/config.edn";
-let f = (await readFile(path)).toString("utf8");
-let conf = edn.toJS(edn.parse(f));
+const local = process.cwd() + "/.cts/config.edn";
+const global = homedir() + "/.cts/config.edn";
 
-function strip(key) {
-  return key.replace(":", "");
-}
+async function setit(p) {
+  let f = (await readFile(p)).toString("utf8");
+  let conf = edn.toJS(edn.parse(f));
 
-function set(c, prefix) {
-  for (const key in c) {
-    let k = strip(key);
-    let is_obj = (c[key] instanceof Object) && !(c[key] instanceof Array);
-    if (is_obj) {
-      set(c[key], `${k}_`);
-    } else {
-      let is_arr = c[key] instanceof Array;
-      let is_bool = typeof c[key] == "boolean";
-      if (is_arr) {
-        c[key] = c[key].join(",");
-      } else if (is_bool) {
-        c[key] = c[key] ? 1 : 0;
+  function strip(key) {
+    return key.replace(":", "");
+  }
+
+  function set(c, prefix) {
+    for (const key in c) {
+      let k = strip(key);
+      let is_obj = (c[key] instanceof Object) && !(c[key] instanceof Array);
+      if (is_obj) {
+        set(c[key], `${k}_`);
+      } else {
+        let is_arr = c[key] instanceof Array;
+        let is_bool = typeof c[key] == "boolean";
+        if (is_arr) {
+          c[key] = c[key].join(",");
+        } else if (is_bool) {
+          c[key] = c[key] ? 1 : 0;
+        }
+
+        let n = `${prefix}${k}`.toUpperCase();
+        process.env[n] = c[key];
       }
-
-      let n = `${prefix}${k}`.toUpperCase();
-      process.env[n] = c[key];
     }
   }
+
+  set(conf, "");
 }
 
-set(conf, "");
+if (await exists(global)) await setit(global);
+if (await exists(local)) await setit(local);

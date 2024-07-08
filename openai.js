a77ed281cc40ec74ec3d019eae1a8ea3cc05ef1d
Rajdeep Malakar <rajdeepm.dev@gmail.com>
2024-07-08 15:50:30 +0530

    config: add OpenAI API URL and model configuration options

--- openai.js
+++ openai.js
@@ -2,7 +2,7 @@ import { ChatGPTAPI } from "chatgpt";
 
 import { encode } from 'gpt-3-encoder';
 import inquirer from "inquirer";
-import { AI_PROVIDER } from "./config.js"
+import { OPENAI_MODEL, OPENAI_API_URL } from "./config.js"
 
 const FEE_PER_1K_TOKENS = 0.02;
 const MAX_TOKENS = 128000;
@@ -10,33 +10,34 @@ const MAX_TOKENS = 128000;
 const FEE_COMPLETION = 0.001;
 
 const openai = {
-  sendMessage: async (input, {apiKey, model}) => {
+  sendMessage: async (input, { apiKey, model }) => {
     console.log("prompting chat gpt...");
     console.log("prompt: ", input);
     const api = new ChatGPTAPI({
       apiKey,
       completionParams: {
-        model: "gpt-4-1106-preview",
+        model: OPENAI_MODEL,
       },
+      apiBaseUrl: OPENAI_API_URL
     });
     const { text } = await api.sendMessage(input);
 
     return text;
   },
 
-  getPromptForSingleCommit: (diff, {commitType, language}) => {
+  getPromptForSingleCommit: (diff, { commitType, language }) => {
 
     return (
       "I want you to act as the author of a commit message in git." +
       `I'll enter a git diff, and your job is to convert it into a useful commit message in ${language} language` +
       (commitType ? ` with commit type '${commitType}'. ` : ". ") +
       "Do not preface the commit with anything, use the present tense, return the full sentence, and use the conventional commits specification (<type in lowercase>: <subject>): " +
-      '\n\n'+
+      '\n\n' +
       diff
     );
   },
 
-  getPromptForMultipleCommits: (diff, {commitType, numOptions, language}) => {
+  getPromptForMultipleCommits: (diff, { commitType, numOptions, language }) => {
     const prompt =
       "I want you to act as the author of a commit message in git." +
       `I'll enter a git diff, and your job is to convert it into a useful commit message in ${language} language` +
@@ -53,25 +54,25 @@ const openai = {
     const fee = numTokens / 1000 * FEE_PER_1K_TOKENS + (FEE_COMPLETION * numCompletion);
 
     if (numTokens > MAX_TOKENS) {
-        console.log("The commit diff is too large for the ChatGPT API. Max 4k tokens or ~8k characters. ");
-        return false;
+      console.log("The commit diff is too large for the ChatGPT API. Max 4k tokens or ~8k characters. ");
+      return false;
     }
 
     if (filterFee) {
-        console.log(`This will cost you ~$${+fee.toFixed(3)} for using the API.`);
-        const answer = await inquirer.prompt([
-            {
-                type: "confirm",
-                name: "continue",
-                message: "Do you want to continue 💸?",
-                default: true,
-            },
-        ]);
-        if (!answer.continue) return false;
+      console.log(`This will cost you ~$${+fee.toFixed(3)} for using the API.`);
+      const answer = await inquirer.prompt([
+        {
+          type: "confirm",
+          name: "continue",
+          message: "Do you want to continue 💸?",
+          default: true,
+        },
+      ]);
+      if (!answer.continue) return false;
     }
 
     return true;
-}
+  }
 
 
 };

1e82940513ec301d80035eec6042aaab5cc4fa18
Rajdeep Malakar <rajdeepm.dev@gmail.com>
2024-07-09 14:49:13 +0530

    Move to MPL
    Signed-off-by: Rajdeep Malakar <rajdeepm.dev@gmail.com>

--- filterApi.js
+++ filterApi.js
@@ -1,3 +1,12 @@
+/*
+ * Copyright (c) 2024, Rajdeep Malakar
+ * All rights reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 import { encode } from 'gpt-3-encoder';
 import inquirer from "inquirer";
 import { AI_PROVIDER } from "./config.js"
@@ -8,32 +17,32 @@ const MAX_TOKENS = 4000;
 const FEE_COMPLETION = 0.001;
 
 async function filterApi({ prompt, numCompletion = 1, filterFee }) {
-    if(AI_PROVIDER == 'ollama'){
-        //ollama dont have any limits and is free so we dont need to filter anything
-        return true
-    }
-    const numTokens = encode(prompt).length;
-    const fee = numTokens / 1000 * FEE_PER_1K_TOKENS + (FEE_COMPLETION * numCompletion);
+  if (AI_PROVIDER == 'ollama') {
+    //ollama dont have any limits and is free so we dont need to filter anything
+    return true
+  }
+  const numTokens = encode(prompt).length;
+  const fee = numTokens / 1000 * FEE_PER_1K_TOKENS + (FEE_COMPLETION * numCompletion);
 
-    if (numTokens > MAX_TOKENS) {
-        console.log("The commit diff is too large for the ChatGPT API. Max 4k tokens or ~8k characters. ");
-        return false;
-    }
+  if (numTokens > MAX_TOKENS) {
+    console.log("The commit diff is too large for the ChatGPT API. Max 4k tokens or ~8k characters. ");
+    return false;
+  }
 
-    if (filterFee) {
-        console.log(`This will cost you ~$${+fee.toFixed(3)} for using the API.`);
-        const answer = await inquirer.prompt([
-            {
-                type: "confirm",
-                name: "continue",
-                message: "Do you want to continue 💸?",
-                default: true,
-            },
-        ]);
-        if (!answer.continue) return false;
-    }
+  if (filterFee) {
+    console.log(`This will cost you ~$${+fee.toFixed(3)} for using the API.`);
+    const answer = await inquirer.prompt([
+      {
+        type: "confirm",
+        name: "continue",
+        message: "Do you want to continue 💸?",
+        default: true,
+      },
+    ]);
+    if (!answer.continue) return false;
+  }
 
-    return true;
+  return true;
 };
 
 export { filterApi }
--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 #!/usr/bin/env node
 
+/*
+ * Copyright (c) 2024, Rajdeep Malakar
+ * All rights reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 'use strict'
 import process from "node:process"
 import { exec, execSync } from "node:child_process";
